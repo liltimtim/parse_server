2fa34009164c9bda71f5fe4accbeaf27b7b22b49
Timothy Barrett <timothybarrett@Timothys-MBP.knology.net>
2017-05-30 15:02:56 -0400

    update deploy

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     PARSE_MASTER_KEY: process.env.PARSE_MASTER_KEY || '?Ag4M)ZA*BBpIGL!XIqCe6acB&rcmL',
     PARSE_SERVER_URL: process.env.PARSE_SERVER_URL || 'http://localhost:1337/parse',
     PARSE_ADMIN_USERNAME: process.env.PARSE_ADMIN_USERNAME || 'admin',
-    PARSE_ADMIN_PASSWORD: process.env.PARSE_ADMIN_PASSWORD || 'QAZwsxEDC1234!@#$',
+    PARSE_ADMIN_PASSWORD: process.env.PARSE_ADMIN_PASSWORD || 'password',
     PARSE_DASHBOARD_APP_NAME: process.env.PARSE_DASHBOARD_APP_NAME || 'your_app_id_dashboard',
     PARSE_SERVER_MOUNT: process.env.PARSE_SERVER_MOUNT || '/parse'
 }
