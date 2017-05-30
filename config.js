a4253843a652ed388bb2c226b0aaf917ceac3298
Timothy Barrett <timothybarrett@Timothys-MBP.knology.net>
2017-05-30 15:14:04 -0400

    uipdate

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
     PARSE_APP_ID: process.env.PARSE_APP_ID || 'your_app_id',
     PARSE_MASTER_KEY: process.env.PARSE_MASTER_KEY || '?Ag4M)ZA*BBpIGL!XIqCe6acB&rcmL',
     PARSE_SERVER_URL: process.env.PARSE_SERVER_URL || 'http://localhost:1337/parse',
-    PARSE_ADMIN_USERNAME: process.env.PARSE_ADMIN_USERNAME || 'admin',
-    PARSE_ADMIN_PASSWORD: process.env.PARSE_ADMIN_PASSWORD || 'password',
+    PARSE_ADMIN_USERNAME: process.env.PARSE_ADMIN_DASHBOARD_USERNAME || 'admin',
+    PARSE_ADMIN_PASSWORD: process.env.PARSE_ADMIN_DASHBOARD_PASSWORD || 'password',
     PARSE_DASHBOARD_APP_NAME: process.env.PARSE_DASHBOARD_APP_NAME || 'your_app_id_dashboard',
     PARSE_SERVER_MOUNT: process.env.PARSE_SERVER_MOUNT || '/parse'
 }
