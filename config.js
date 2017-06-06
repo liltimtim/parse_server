9a16adb53ea4f5112228af5552d4e614cbaff920
Timothy Barrett <liltimtim@gmail.com>
2017-06-06 13:42:54 -0400

    Update config.js
    changed config to include client key

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ module.exports = {
     PARSE_ADMIN_USERNAME: process.env.PARSE_ADMIN_DASHBOARD_USERNAME || 'admin',
     PARSE_ADMIN_PASSWORD: process.env.PARSE_ADMIN_DASHBOARD_PASSWORD || 'password',
     PARSE_DASHBOARD_APP_NAME: process.env.PARSE_DASHBOARD_APP_NAME || 'yourappname',
-    PARSE_SERVER_MOUNT: process.env.PARSE_SERVER_MOUNT || '/parse'
+    PARSE_SERVER_MOUNT: process.env.PARSE_SERVER_MOUNT || '/parse',
+    PARSE_CLIENT_KEY: process.env.PARSE_CLIENT_KEY || 'client_key'
 }
