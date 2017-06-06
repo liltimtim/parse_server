583ee08c7b35d95f09bfeed31c5e71335d264cc6
Timothy Dillman <tim@launchpeer.com>
2017-06-06 15:41:08 -0400

    enabled sample push notification certs and example

--- config.js
+++ config.js
@@ -7,5 +7,10 @@ module.exports = {
     PARSE_ADMIN_PASSWORD: process.env.PARSE_ADMIN_DASHBOARD_PASSWORD || 'password',
     PARSE_DASHBOARD_APP_NAME: process.env.PARSE_DASHBOARD_APP_NAME || 'yourappname',
     PARSE_SERVER_MOUNT: process.env.PARSE_SERVER_MOUNT || '/parse',
-    PARSE_CLIENT_KEY: process.env.PARSE_CLIENT_KEY || 'client_key'
+    PARSE_CLIENT_KEY: process.env.PARSE_CLIENT_KEY || 'client_key',
+    GCM_SENDER_ID: process.env.GCM_SENDER_ID || 'sender_id',
+    GCM_API_KEY: process.env.GCM_API_KEY || 'gcm_api_key',
+    APPLE_BUNDLE_ID: process.env.APPLE_BUNDLE_ID || 'com.lp.example.app',
+    APNS_PASSPHRASE: process.env.APNS_PASSPHRASE || 'password',
+    APNS_PUSH_PRODUCTION: process.env.APNS_PUSH_PRODUCTION || false
 }
--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const config = require('./config');
 const path = require('path');
 const PORT = process.env.PORT || 1337;
 const ParseDashboard = require('parse-dashboard');
-
+const productionDirectoryLocation = __dirname + '/apns_push_certs/production/apns_production.p12';
+const sandboxDirectoryLocation = __dirname + '/apns_push_certs/sandbox/apns_sandbox.p12';
+console.log(productionDirectoryLocation);
 /**
  * Parse server options
  */
@@ -21,7 +23,28 @@ const options = {
     appId: config.PARSE_APP_ID,
     masterKey: config.PARSE_MASTER_KEY,
     serverURL: config.PARSE_SERVER_URL,
-    clientKey: config.P
+    clientKey: config.PARSE_CLIENT_KEY,
+    push: {
+        ios: [
+            //create and support both sandbox and production
+            {
+                pfx:sandboxDirectoryLocation,
+                passphrase: config.APNS_PASSPHRASE,
+                bundleId: config.APPLE_BUNDLE_ID,
+                production: false
+            },
+
+            // issue created on github: https://github.com/parse-community/parse-server/issues/3911
+            // setting value to true even though its a production cert crashes server
+            
+            {
+                pfx:productionDirectoryLocation,
+                passphrase: config.APNS_PASSPHRASE,
+                bundleId: config.APPLE_BUNDLE_ID,
+                production: false
+            }
+        ]
+    }
 }
 const api = new ParseServer(options);
 const dashboard = new ParseDashboard({
